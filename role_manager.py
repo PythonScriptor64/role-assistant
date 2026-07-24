4a152af20ad57516e86350ff0b113a9150ebfe56
PythonScriptor64 <139090143+PythonScriptor64@users.noreply.github.com>
2026-07-24 12:26:07 -0700

    check if role object is none

--- role_manager.py
+++ role_manager.py
@@ -10,8 +10,15 @@ async def give_role(interaction: discord.Interaction, role_id: int | None):
             ephemeral=True
         )
         return False
-    
+
     role = interaction.user.guild.get_role(role_id)
+    if role is None:
+        await interaction.response.send_message(
+            "Role does not exist; contact an administrator.",
+            ephemeral=True
+        )
+        return False
+    
     if role in interaction.user.roles:
         await interaction.response.send_message(
             "You already have this role.",
@@ -36,6 +43,13 @@ async def take_role(interaction: discord.Interaction, role_id: int | None):
         return False
     
     role = interaction.user.guild.get_role(role_id)
+    if role is None:
+            await interaction.response.send_message(
+                "Role does not exist; contact an administrator.",
+                ephemeral=True
+            )
+            return False
+    
     if role not in interaction.user.roles:
         await interaction.response.send_message(
             "You don't have this role.",
